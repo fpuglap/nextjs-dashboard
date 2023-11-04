ee30836eb9835cd6e7f9f02b1f53c04e5945e782
Facundo Pugliese <fpugliese09@gmail.com>
2023-11-04 03:58:14 -0300

    Streaming the data

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,18 +15,25 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card title='Collected' value={totalPaidInvoices} type='collected' />
+      <Card title='Pending' value={totalPendingInvoices} type='pending' />
+      <Card title='Total Invoices' value={numberOfInvoices} type='invoices' />
       <Card
-        title="Total Customers"
+        title='Total Customers'
         value={numberOfCustomers}
-        type="customers"
-      /> */}
+        type='customers'
+      />
     </>
   );
 }
@@ -42,10 +50,10 @@ export function Card({
   const Icon = iconMap[type];
 
   return (
-    <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
-      <div className="flex p-4">
-        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        <h3 className="ml-2 text-sm font-medium">{title}</h3>
+    <div className='rounded-xl bg-gray-50 p-2 shadow-sm'>
+      <div className='flex p-4'>
+        {Icon ? <Icon className='h-5 w-5 text-gray-700' /> : null}
+        <h3 className='ml-2 text-sm font-medium'>{title}</h3>
       </div>
       <p
         className={`${lusitana.className}

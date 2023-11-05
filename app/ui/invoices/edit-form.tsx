794098bfb244cf2bdc0590bd8a211962a68362b1
Facundo Pugliese <fpugliese09@gmail.com>
2023-11-05 00:42:21 -0300

    Mutating data

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,22 +18,24 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
   return (
-    <form>
-      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+    <form action={updateInvoiceWithId}>
+      <div className='rounded-md bg-gray-50 p-4 md:p-6'>
         {/* Customer Name */}
-        <div className="mb-4">
-          <label htmlFor="customer" className="mb-2 block text-sm font-medium">
+        <div className='mb-4'>
+          <label htmlFor='customer' className='mb-2 block text-sm font-medium'>
             Choose customer
           </label>
-          <div className="relative">
+          <div className='relative'>
             <select
-              id="customer"
-              name="customerId"
-              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              id='customer'
+              name='customerId'
+              className='peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500'
               defaultValue={invoice.customer_id}
             >
-              <option value="" disabled>
+              <option value='' disabled>
                 Select a customer
               </option>
               {customers.map((customer) => (
@@ -41,81 +44,81 @@ export default function EditInvoiceForm({
                 </option>
               ))}
             </select>
-            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+            <UserCircleIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500' />
           </div>
         </div>
 
         {/* Invoice Amount */}
-        <div className="mb-4">
-          <label htmlFor="amount" className="mb-2 block text-sm font-medium">
+        <div className='mb-4'>
+          <label htmlFor='amount' className='mb-2 block text-sm font-medium'>
             Choose an amount
           </label>
-          <div className="relative mt-2 rounded-md">
-            <div className="relative">
+          <div className='relative mt-2 rounded-md'>
+            <div className='relative'>
               <input
-                id="amount"
-                name="amount"
-                type="number"
+                id='amount'
+                name='amount'
+                type='number'
                 defaultValue={invoice.amount}
-                placeholder="Enter USD amount"
-                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                placeholder='Enter USD amount'
+                className='peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500'
               />
-              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <CurrencyDollarIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
             </div>
           </div>
         </div>
 
         {/* Invoice Status */}
         <fieldset>
-          <legend className="mb-2 block text-sm font-medium">
+          <legend className='mb-2 block text-sm font-medium'>
             Set the invoice status
           </legend>
-          <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
-            <div className="flex gap-4">
-              <div className="flex items-center">
+          <div className='rounded-md border border-gray-200 bg-white px-[14px] py-3'>
+            <div className='flex gap-4'>
+              <div className='flex items-center'>
                 <input
-                  id="pending"
-                  name="status"
-                  type="radio"
-                  value="pending"
+                  id='pending'
+                  name='status'
+                  type='radio'
+                  value='pending'
                   defaultChecked={invoice.status === 'pending'}
-                  className="h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
+                  className='h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600'
                 />
                 <label
-                  htmlFor="pending"
-                  className="ml-2 flex items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600 dark:text-gray-300"
+                  htmlFor='pending'
+                  className='ml-2 flex items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600 dark:text-gray-300'
                 >
-                  Pending <ClockIcon className="h-4 w-4" />
+                  Pending <ClockIcon className='h-4 w-4' />
                 </label>
               </div>
-              <div className="flex items-center">
+              <div className='flex items-center'>
                 <input
-                  id="paid"
-                  name="status"
-                  type="radio"
-                  value="paid"
+                  id='paid'
+                  name='status'
+                  type='radio'
+                  value='paid'
                   defaultChecked={invoice.status === 'paid'}
-                  className="h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
+                  className='h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600'
                 />
                 <label
-                  htmlFor="paid"
-                  className="ml-2 flex items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white dark:text-gray-300"
+                  htmlFor='paid'
+                  className='ml-2 flex items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white dark:text-gray-300'
                 >
-                  Paid <CheckIcon className="h-4 w-4" />
+                  Paid <CheckIcon className='h-4 w-4' />
                 </label>
               </div>
             </div>
           </div>
         </fieldset>
       </div>
-      <div className="mt-6 flex justify-end gap-4">
+      <div className='mt-6 flex justify-end gap-4'>
         <Link
-          href="/dashboard/invoices"
-          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+          href='/dashboard/invoices'
+          className='flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200'
         >
           Cancel
         </Link>
-        <Button type="submit">Edit Invoice</Button>
+        <Button type='submit'>Edit Invoice</Button>
       </div>
     </form>
   );

03141c89fd5f01b7f4d607700565d806ceefcd80
Facundo Pugliese <fpugliese09@gmail.com>
2023-11-10 02:59:06 -0300

    Adding metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,18 @@
-import "@/app/ui/global.css";
-import { inter } from "@/app/ui/fonts";
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme',
+    default: 'Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://nextjs-dashboard-fpuglap.vercel.app'),
+  keywords: ['Next.js', 'React', 'JavaScript'],
+  creator: 'Facundo Pugliese',
+  publisher: 'Facundo Pugliese',
+};
 
 export default function RootLayout({
   children,

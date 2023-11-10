9e2aa1b33e79bb82d6b4bd9999d7be8811dc0309
Facundo Pugliese <fpugliese09@gmail.com>
2023-11-10 02:12:25 -0300

    Update user's password

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -5,7 +5,8 @@ const users = [
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
     name: 'User',
     email: 'user@nextmail.com',
-    password: '123456',
+    // openssl rand -base64 32
+    password: '123458DLIsNObl5oU31egHZ5O+w6peS7pz30bZiZBHIDNmpM=6',
   },
 ];
 
